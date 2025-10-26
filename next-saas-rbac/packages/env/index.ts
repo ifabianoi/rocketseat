4a36b42ed4bc6abee5b88e182f3765ce72f83d91
Fabiano Pereira <fabiano.pereira@corebiz.ag>
2025-10-26 16:05:53 -0300

    chore(): fix eslint

--- next-saas-rbac/packages/env/index.ts
+++ next-saas-rbac/packages/env/index.ts
@@ -22,7 +22,7 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
     GITHUB_OAUTH_CLIENT_REDIRECT_URI:
       process.env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
-    
+
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   emptyStringAsUndefined: true,
